aa7bb374296208472943fc98d3f7bb1bbb980d53
Sam Venter <sventer444@gmail.com>
2024-03-03 18:45:26 -0500

    fixed pc window

--- src/components/PcBoxes.vue
+++ src/components/PcBoxes.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="pc-boxes bg-pokemon-dark p-4 text-white text-center max-w-full max-h-full overflow-hidden">
-      <h2 class="text-2xl font-semibold">PC</h2>
-  
       <!-- Display Pokémon icons in a 5x6 grid -->
-      <div class="grid grid-cols-6 gap-2 bg-pokemon-dark-shaded p-5 mb-4">
+      <div class="grid grid-cols-6 gap-2 bg-pokemon-dark-shaded p-5">
         <div
           v-for="(pokemonDetails, index) in pcDisplay"
           :key="index"
@@ -91,55 +89,54 @@
   </script>
   
   <style scoped>
-  /* Apply global Tailwind CSS styles */
-  .pc-boxes {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start; /* Align content to the start to move it to the top */
-    border-radius: 0.5rem;
-    color: #ecf0f1; /* White text color */
-    font-size: 1rem; /* 16px */
-    line-height: 1.5;
-    max-width: 100%;
-    height: calc(100vh - 120px); /* Adjust as needed based on the height of top and bottom navbars */
-    overflow-y: auto;
-    margin-bottom: 0; /* Remove margin at the bottom */
-    flex-grow: 2; /* Adjust flex-grow as needed to fill the available space */
-  }
-  
-  .text-2xl {
-    font-size: 1.5rem; /* 24px */
-  }
-  
-  .grid {
-    display: grid;
-  }
-  
-  .grid-cols-6 {
-    grid-template-columns: repeat(6, minmax(0, 1fr));
-  }
-  
-  .gap-2 {
-    gap: 0.5rem;
-  }
-  
-  .p-2 {
-    padding: 0.25rem;
-  }
-  
-  .w-20,
-  .h-20 {
-    width: 5rem;
-    height: 5rem;
-  }
-  
-  .cursor-pointer {
-    cursor: pointer;
-  }
-  
-  .selected-slot {
-    border-color: #3498db; /* Blue border color for selected slots */
-  }
-  </style>
-  
+/* Apply global Tailwind CSS styles */
+.pc-boxes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center; /* Center content vertically */
+  border-radius: 0.5rem;
+  color: #ecf0f1;
+  font-size: 1rem;
+  line-height: 1.5;
+  max-width: 100%;
+  height: calc(100vh - 120px); /* Adjust as needed based on the height of top and bottom navbars */
+  overflow-y: auto;
+  margin-bottom: 0;
+  flex-grow: 2;
+}
+
+.text-2xl {
+  font-size: 1.5rem; /* 24px */
+}
+
+.grid {
+  display: grid;
+}
+
+.grid-cols-6 {
+  grid-template-columns: repeat(6, minmax(0, 1fr));
+}
+
+.gap-2 {
+  gap: 0.5rem;
+}
+
+.p-2 {
+  padding: 0.25rem;
+}
+
+.w-20,
+.h-20 {
+  width: 5rem;
+  height: 5rem;
+}
+
+.cursor-pointer {
+  cursor: pointer;
+}
+
+.selected-slot {
+  border-color: #3498db; /* Blue border color for selected slots */
+}
+</style>
